b29a4c7928d857c8d4d448dadf087c7eefc5d016
AndrewVerem <54248444+AndrewVerem@users.noreply.github.com>
2020-08-07 20:54:15 +0700

    Add files SpaceX

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const instance = new vidbg(".video", {
+  mp4: "../video/world.mp4",
+  webm: "../video/world.webm",
+  poster: "../video/poster.jpg",
+  overlay: false,
+});
+
+const rellax = new Rellax(".rocket");
+window.addEventListener("resize", () => {
+  if (document.body.clientWidth < 576) {
+    rellax.destroy();
+  }
+});
+
+AOS.init();
